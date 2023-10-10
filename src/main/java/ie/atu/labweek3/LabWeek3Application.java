bbc1785aaeddb80b0fad1b708510133b1024b83f
Denis <G00403126@atu.ie>
2023-10-10 15:22:16 +0100

    ...

---
+++ src/main/java/ie/atu/labweek3/LabWeek3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.labweek3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabWeek3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabWeek3Application.class, args);
+    }
+
+}
